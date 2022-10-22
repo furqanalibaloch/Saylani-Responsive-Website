6fff2f024b7ecfe7d03a6535578fab00cccdce9f
furqanalibaloch <99679037+furqanalibaloch@users.noreply.github.com>
2022-10-22 16:32:41 +0500

    add responsive website

---
+++ app.js
@@ -0,0 +1,42 @@
+$('#count-example').jQuerySimpleCounter({
+    
+      start:  0,
+    
+      end:    5000,
+    
+    });
+
+    $('#count-example').jQuerySimpleCounter({
+        
+         
+        
+          // start number
+        
+          start:  0,
+        
+         
+        
+          // end number
+        
+          end:    5000,
+        
+         
+        
+          // easing effect
+        
+          easing:'swing',
+        
+         
+    
+          // duration time in ms
+        
+          duration: 400,
+    
+         
+        
+          // callback function
+        
+          complete:''
+    
+        });
+        
